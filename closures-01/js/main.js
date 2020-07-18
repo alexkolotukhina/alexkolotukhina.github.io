867b0d0f20844defad334c8529fb1bcc0e991057
Alexandra Kolotukhina <oleksandra.kolotukhina@beetroot.se>
2020-07-18 20:34:46 +0300

    Done task 01.

--- closures-01/js/main.js
+++ closures-01/js/main.js
@@ -11,11 +11,28 @@
    
 */
 
-const list = document.getElementById("list"),
-  els = list.getElementsByTagName("li");
+const listVar = document.getElementById("list-var"),
+  elsVar = listVar.getElementsByTagName("li");
 
-for (var i = 0, len = els.length; i < len; i++) {
-  els[i].onclick = function () {
-    alert(i);
+for (var i = 0, len = elsVar.length; i < len; i++) {
+  elsVar[i].onclick = function () {
+    var index = this.innerText;
+    function indexVar() {
+      alert(index);
+    }
+    indexVar();
+  };
+}
+
+const listLet = document.getElementById("list-let"),
+  elsLet = listLet.getElementsByTagName("li");
+
+for (let i = 0, len = elsLet.length; i < len; i++) {
+  elsLet[i].onclick = function () {
+    var index = i + 1;
+    function indexLet() {
+      alert(index);
+    }
+    indexLet();
   };
 }
