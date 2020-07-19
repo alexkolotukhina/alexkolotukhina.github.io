ac491b88518c79be309bab7888b47e4ab468eedb
Alexandra Kolotukhina <oleksandra.kolotukhina@beetroot.se>
2020-07-19 13:54:26 +0300

    Add regular expressions 01 task.

---
+++ regular-01/js/main.js
@@ -0,0 +1,22 @@
+/*  Начальные 3 цифры в каждом номере- это код оператора
+ Сделать вывод  в консоль таким
+(063) 111 1234
+(333) 456 8768
+(444) 657 5547
+*/
+let st = `063-111-1234
+(333) 456-8768
+4446575547`;
+
+let form = document.querySelector(".number-format");
+let input = form.querySelector(".number-format__input");
+let result = document.querySelector(".number-format__result span");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let regular = /\(?([0-9]{3})[\)]?[-]?[\s]?([0-9]{3})[-]?[\s]?([0-9]{2})[-]?[\s]?([0-9]{2})/g;
+  let phoneVal = input.value;
+  let formating = phoneVal.replace(regular, "($1) $2 $3$4");
+  result.innerHTML = formating;
+  input.value = "";
+});
